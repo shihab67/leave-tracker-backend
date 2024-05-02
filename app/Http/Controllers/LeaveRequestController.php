e67a04b39a6f19117a257433106ff894b8fa64e4
Sayedur Rahman <s.rahman3443@gmail.com>
2024-05-02 20:55:42 +0600

    Update LeaveRequestController.php

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -13,7 +13,7 @@ class LeaveRequestController extends Controller
     public function store(Request $request)
     {
         $validator                  = Validator::make($request->all(), [
-            'leave_type_id'         => 'required|exists:leave_types,id',
+            'leave_type'         => 'required|exists:leave_types,id',
             'start_date'            => 'required|date',
             'end_date'              => 'required|date|after_or_equal:start_date',
             'reason'                => 'required|string',
@@ -23,32 +23,39 @@ class LeaveRequestController extends Controller
             return response(['errors' => $validator->errors()->all()], 422);
         }
 
-        $leaveRequest = new LeaveRequest();
-        $leaveRequest->user_id = $request->user()->id;
-        $leaveRequest->leave_type_id = $request->leave_type_id;
-        $leaveRequest->start_date = date('Y-m-d', strtotime($request->start_date));
-        $leaveRequest->end_date = date('Y-m-d', strtotime($request->end_date));
-        $leaveRequest->reason = $request->reason;
-        $leaveRequest->status = 'pending';
-        $leaveRequest->save();
+        try {
+            $leaveRequest = new LeaveRequest();
+            $leaveRequest->user_id = $request->user()->id;
+            $leaveRequest->leave_type_id = $request->leave_type;
+            $leaveRequest->start_date = date('Y-m-d', strtotime($request->start_date));
+            $leaveRequest->end_date = date('Y-m-d', strtotime($request->end_date));
+            $leaveRequest->reason = $request->reason;
+            $leaveRequest->status = 'pending';
+            $leaveRequest->save();
 
-        //send mail starts
-        $details = [
-            'body' => 'Leave request created successfully. Wait for approval.',
-            'reason' => $leaveRequest->reason,
-            'type' => $leaveRequest->leaveType->name,
-            'start_date' => date('d M Y', strtotime($leaveRequest->start_date)),
-            'end_date' => date('d M Y', strtotime($leaveRequest->end_date)),
-            'status' => ucfirst($leaveRequest->status),
-        ];
+            //send mail starts
+            $details = [
+                'body' => 'Leave request created successfully. Wait for approval.',
+                'reason' => $leaveRequest->reason,
+                'type' => $leaveRequest->leaveType->name,
+                'start_date' => date('d M Y', strtotime($leaveRequest->start_date)),
+                'end_date' => date('d M Y', strtotime($leaveRequest->end_date)),
+                'status' => ucfirst($leaveRequest->status),
+            ];
 
-        Mail::to($request->user()->email)->send(new LeaveRequestMail($details));
-        //send mail ends
+            Mail::to($request->user()->email)->send(new LeaveRequestMail($details));
+            //send mail ends
 
-        return response(
-            ['message' => 'Leave request created successfully'],
-            201
-        );
+            return response(
+                [
+                    'status' => 'success',
+                    'message' => 'Leave request created successfully'
+                ],
+                201
+            );
+        } catch (\Throwable $th) {
+            return response(['errors' => $th->getMessage()], 422);
+        }
     }
 
     public function approve(Request $request)
